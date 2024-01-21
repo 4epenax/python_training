7d79fa9c130d45a14baa4fd344e27d6c9677ceb5
4epenax <a4epenax@ya.ru>
2024-01-21 03:54:16 +0300

    тест создания контакта переведён с фреймворка unittest на pytest

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,27 +1,23 @@
 # -*- coding: utf-8 -*-
-import unittest
+import pytest
 from contact import Contact
 from application import Application
 
 
-class TestAddContact(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
-
-    def test_add_contact(self):
-        self.app.login(username="admin", password="secret")
-        self.app.create_contact(Contact(firstname="qwe", middlename="wea", lastname="qwe", nickname="123", title="asd",
-                                        company="asd", address="fsd", homephone="123", mobilephone="asd",
-                                        workphone="zcx",
-                                        fax="312", email="qwe1",
-                                        email2="123q", email3="e12e", homepage="31wea", bday="13", bmonth="September",
-                                        byear="1990",
-                                        aday="18", amonth="12", ayear="2020"))
-        self.app.logout()
-
-    def tearDown(self):
-        self.app.destroy()
+@pytest.fixture()
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_add_contact(app):
+    app.login(username="admin", password="secret")
+    app.create_contact(Contact(firstname="qwe", middlename="wea", lastname="qwe", nickname="123", title="asd",
+                               company="asd", address="fsd", homephone="123", mobilephone="asd",
+                               workphone="zcx",
+                               fax="312", email="qwe1",
+                               email2="123q", email3="e12e", homepage="31wea", bday="13", bmonth="September",
+                               byear="1990",
+                               aday="18", amonth="12", ayear="2020"))
+    app.logout()
