522d14771d42da5e05dd8332eaf5072d52e15e22
4epenax <a4epenax@ya.ru>
2024-01-29 12:13:57 +0300

    добавлена проверка предусловия в тесте модификации контакта

--- test/test_mod_contact.py
+++ test/test_mod_contact.py
@@ -3,6 +3,8 @@ from model.contact import Contact
 
 
 def test_modify_contact(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="Temp"))
     app.contact.open_home_page()
     app.contact.edit_contact()
     app.contact.fill_contact_form(
@@ -10,6 +12,6 @@ def test_modify_contact(app):
                 title="t1 change", company="c2 change", address="a3 change", homephone="hp change",
                 mobilephone="mp change", workphone="wp change", fax="f4 change", email="el change", email2="el2 change",
                 email3="e3 change", homepage="hp change", bday="18", bmonth="August", byear="1985", aday="9",
-                amonth="8", ayear="2011"))
+                amonth="April", ayear="2011"))
     app.contact.update_contact()
     app.contact.return_to_home_page()
