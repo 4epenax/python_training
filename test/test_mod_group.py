14a43fb0d9aa8a467f5f039c3b96394be2fa7568
4epenax <a4epenax@ya.ru>
2024-02-03 08:50:18 +0300

    в тесты добавлены проверки количества групп до и после удаления/модификации

--- test/test_mod_group.py
+++ test/test_mod_group.py
@@ -5,20 +5,26 @@ from model.group import Group
 def test_modify_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="new"))
+    old_groups = app.group.get_group_list()
     app.group.open_groups_page()
     app.group.selected_first_group()
     app.group.edit()
     app.group.fill_form(Group(name="tg change", header="hg change", footer="fg change"))
     app.group.submit_modification()
     app.group.return_to_groups_page()
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_modify_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="new"))
+    old_groups = app.group.get_group_list()
     app.group.open_groups_page()
     app.group.selected_first_group()
     app.group.edit()
     app.group.fill_form(Group(name="New group"))
     app.group.submit_modification()
     app.group.return_to_groups_page()
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
