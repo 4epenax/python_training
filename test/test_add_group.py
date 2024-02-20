0ff6ec47023cb0f83fa0df507ca44122816cbd8c
4epenax <a4epenax@ya.ru>
2024-02-20 18:02:15 +0300

    доработаны conftest и generator, реализована динамическая генерация тестов

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,10 +2,11 @@
 from model.group import Group
 
 
-def test_add_group(app, data_groups):
+def test_add_group(app, json_groups):
+    group = json_groups
     old_groups = app.group.get_group_list()
-    app.group.create(data_groups)
+    app.group.create(group)
     assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()
-    old_groups.append(data_groups)
+    old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
