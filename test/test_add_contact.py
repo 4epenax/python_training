86489ce6a2e4d60a6d3aadb2e1dcbc2fe1f7516a
4epenax <a4epenax@ya.ru>
2024-02-20 17:31:14 +0300

    доработан conftest, на пути к динамической генерации тестов и контакты тоже

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
-import pytest
-from data.contacts import testdata
 
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_contact(app, contact):
+
+def test_add_contact(app, data_contacts):
     old_contacts = app.contact.get_contact_list()
-    app.contact.create(contact)
+    app.contact.create(data_contacts)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    old_contacts.append(contact)
+    old_contacts.append(data_contacts)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts,
                                                                  key=Contact.id_or_max)
