4bb2ab4dab4f982867cd546823317b560a087b01
4epenax <a4epenax@ya.ru>
2024-02-28 00:23:58 +0300

    доработан тест модификации группы, теперь данные сравниваются с БД

--- test/test_mod_group.py
+++ test/test_mod_group.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
-from random import randrange
+import random
 
 
-def test_modify_group(app):
-    if app.group.count() == 0:
+def test_modify_group(app, db):
+    if len(db.get_group_list()) == 0:
         app.group.create(Group(name="new"))
-    old_groups = app.group.get_group_list()
-    index = randrange(len(old_groups))
+    old_groups = db.get_group_list()
+    group = random.choice(old_groups)
     group = Group(name="tg change", header="hg change", footer="fg change")
-    group.id = old_groups[index].id
+    group.id = old_groups[0].id
     app.group.open_groups_page()
-    app.group.selected_group_by_index(index)
+    app.group.selected_group_by_id(group.id)
     app.group.edit()
     app.group.fill_form(group)
     app.group.submit_modification()
     app.group.return_to_groups_page()
     assert len(old_groups) == app.group.count()
-    new_groups = app.group.get_group_list()
-    old_groups[index] = group
+    new_groups = db.get_group_list()
+    old_groups[0] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
