7d00cf73896de92e1270715e848b5f858ffd6b6a
4epenax <a4epenax@ya.ru>
2024-02-27 23:02:28 +0300

    доработан тест создания контакта, теперь данные сравниваются с БД, также доработан тест сравнения БД и ui (добавлена функция сравнивающая списки контактов)

--- fixture/db.py
+++ fixture/db.py
@@ -1,5 +1,6 @@
 import pymysql.cursors
 from model.group import Group
+from model.contact import Contact
 
 
 class DbFixture:
@@ -9,16 +10,40 @@ class DbFixture:
         self.name = name
         self.user = user
         self.password = password
-        self.connection = pymysql.connect(host=host, database=name, user=user, password=password, autocommit=True)
+        self.connection = pymysql.connect(host=host, database=name, user=user,
+                                          password=password, autocommit=True)
 
     def get_group_list(self):
         list = []
         cursor = self.connection.cursor()
         try:
-            cursor.execute("select group_id, group_name, group_header, group_footer from group_list")
+            cursor.execute(
+                "select group_id, group_name, group_header, group_footer from group_list")
             for row in cursor:
                 (id, name, header, footer) = row
                 list.append(Group(id=str(id), name=name, header=header, footer=footer))
+        finally:
+            cursor.close()
+        return list
+
+    def get_contact_list(self):
+        list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select id, firstname, middlename, lastname, nickname, title, company,"
+                           "address, home, mobile, work, fax, email, email2, email3, homepage,"
+                           "bday, bmonth, byear, aday, amonth, ayear from addressbook")
+            for row in cursor:
+                (id, firstname, middlename, lastname, nickname, title, company, address, home,
+                 mobile, work, fax, email, email2, email3, homepage, bday, bmonth, byear, aday,
+                 amonth, ayear) = row
+                list.append(
+                    Contact(id=str(id), firstname=firstname, middlename=middlename, lastname=lastname,
+                            nickname=nickname, title=title, company=company, address=address,
+                            homephone=home, mobilephone=mobile, workphone=work,
+                            fax=fax, email=email, email2=email2, email3=email3, homepage=homepage,
+                            bday=bday, bmonth=bmonth, byear=byear, aday=aday, amonth=amonth,
+                            ayear=ayear))
         finally:
             cursor.close()
         return list
