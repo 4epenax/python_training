051abd6aa0a1c8fc2dcc6ea476dabe55835ad9d4
4epenax <a4epenax@ya.ru>
2024-02-28 02:20:54 +0300

    в тесты добавлены отключаемые проверки(ui)

--- test/test_mod_contact.py
+++ test/test_mod_contact.py
@@ -3,7 +3,7 @@ from model.contact import Contact
 import random
 
 
-def test_modify_contact(app, db):
+def test_modify_contact(app, db, check_ui):
     if len(db.get_contact_list()) == 0:
         app.contact.create(Contact(firstname="Temp"))
     old_contacts = db.get_contact_list()
@@ -24,3 +24,6 @@ def test_modify_contact(app, db):
     new_contacts = db.get_contact_list()
     old_contacts[0] = contact
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+    if check_ui:
+        assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(),
+                                                                     key=Contact.id_or_max)
--- test/test_mod_group.py
+++ test/test_mod_group.py
@@ -3,7 +3,7 @@ from model.group import Group
 import random
 
 
-def test_modify_group(app, db):
+def test_modify_group(app, db, check_ui):
     if len(db.get_group_list()) == 0:
         app.group.create(Group(name="new"))
     old_groups = db.get_group_list()
@@ -20,3 +20,6 @@ def test_modify_group(app, db):
     new_groups = db.get_group_list()
     old_groups[0] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(),
+                                                                 key=Group.id_or_max)
