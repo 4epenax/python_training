2dfc0c299166c43a3373f4525b257dd8a4a108e8
4epenax <a4epenax@ya.ru>
2024-03-05 05:50:39 +0300

    доработан вспомогательный класс orm

--- fixture/orm.py
+++ fixture/orm.py
@@ -12,12 +12,14 @@ class ORMFixture:
         name = Optional(str, column='group_name')
         header = Optional(str, column='group_header')
         footer = Optional(str, column='group_footer')
+        contacts = Set(lambda: ORMFixture.ORMContact, table="address_in_groups", column="id", reverse="groups", lazy=True)
 
     class ORMContact(db.Entity):
         _table_ = 'addressbook'
         id = PrimaryKey(int, column='id')
         firstname = Optional(str, column='firstname')
         lastname = Optional(str, column='lastname')
+        groups = Set(lambda: ORMFixture.ORMGroup, table="address_in_groups", column="group_id", reverse="contacts", lazy=True)
 
     def __init__(self, host, name, user, password):
         self.db.bind('mysql', host=host, database=name, user=user, password=password)
@@ -41,3 +43,14 @@ class ORMFixture:
     @db_session
     def get_contact_list(self):
         return self.convert_contacts_to_model(select(c for c in ORMFixture.ORMContact))
+
+    def get_orm_group(self, group):
+        return list(select(g for g in ORMFixture.ORMGroup if g.id == group.id))[0]
+
+    @db_session
+    def get_contacts_in_group(self, group):
+        return self.convert_contacts_to_model(self.get_orm_group(group).contacts)
+
+    @db_session
+    def get_contacts_not_in_group(self, group):
+        return self.convert_contacts_to_model(select(c for c in ORMFixture.ORMContact if self.get_orm_group(group) not in c.groups))
